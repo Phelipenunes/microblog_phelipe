c10ca30c3043cc1c2b699b5cad4cf55380f9b453
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-10-10 08:56:38 -0300

    Update Usuario.php

--- src/Usuario.php
+++ src/Usuario.php
@@ -65,4 +65,28 @@ class Usuario {
 
         return $this;
     }
+
+    /**
+     * Get the value of id
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @param int $id
+     *
+     * @return self
+     */
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
 }
