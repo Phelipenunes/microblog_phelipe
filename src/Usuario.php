1a56935c7fdbc240e6f785f35b3b66a3a004e71a
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-10-10 09:09:19 -0300

    Método inserir

--- src/Usuario.php
+++ src/Usuario.php
@@ -14,6 +14,33 @@ class Usuario {
         $this->conexao = Banco::conecta();
     }
 
+    /* Métodos para rotinas de CRUD no Banco */
+
+    // INSERT de Usuario
+    public function inserir():void {
+        $sql = "INSERT INTO usuarios(nome, email, senha, tipo)
+                VALUES(:nome, :email, :senha, :tipo)";
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":nome", $this->nome, PDO::PARAM_STR);
+            $consulta->bindValue(":email", $this->email, PDO::PARAM_STR);
+            $consulta->bindValue(":senha", $this->senha, PDO::PARAM_STR);
+            $consulta->bindValue(":tipo", $this->tipo, PDO::PARAM_STR);
+            $consulta->execute();
+        } catch (Exception $erro) {
+            die("Erro ao inserir usuário: ".$erro->getMessage());
+        }
+    }
+
+
+
+
+
+
+
+
+
     public function getNome(): string
     {
         return $this->nome;
